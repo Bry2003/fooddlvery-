41993621c01873cef8adb62eccdfb88c2786e82f
Bryan Caballero <164503688+Bry2003@users.noreply.github.com>
2024-10-09 10:18:29 +0800

    Add files via upload

---
+++ FoodDelivery.Models/Food.cs
@@ -0,0 +1,11 @@
+﻿namespace FoodDelivery.Models
+{
+    public class Food
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
+
